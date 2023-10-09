26604b8e68dd975e9c3f4be27b4f0aab813d7da9
daniel-dierich <147144836+daniel-dierich@users.noreply.github.com>
2023-10-09 13:46:18 +0200

    Update check-spelling-code.js

--- lint_functions/check-spelling-code.js
+++ lint_functions/check-spelling-code.js
@@ -5,10 +5,12 @@ const separatorsRegex = /\s/     // any whitespace
 
 export default (input) => {
   console.log(input);
-  const words = input.replace(/`/g, '').split(separatorsRegex);
-  const mistakes = words
-    .filter((word) => !exceptions.includes(word))
-    .filter((word) => spellChecker.isMisspelled(word));
+  if (input != undefined){
+    const words = input.replace(/`/g, '').split(separatorsRegex);
+    const mistakes = words
+      .filter((word) => !exceptions.includes(word))
+      .filter((word) => spellChecker.isMisspelled(word));
+  }
 
   if (mistakes.length > 0) {
     return [{
