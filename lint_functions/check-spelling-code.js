ed1f6277459f8f323730b20a2ca59b0f8c7848c2
daniel-dierich <147144836+daniel-dierich@users.noreply.github.com>
2023-10-09 12:39:31 +0200

    Update check-spelling-code.js

--- lint_functions/check-spelling-code.js
+++ lint_functions/check-spelling-code.js
@@ -1,18 +1,18 @@
 const spellChecker = require('spellchecker');
-const exceptions = ["bic","datetime","gt","gte","icontains","iban","idempotency","isnull","lt","lte","md5","mimetype","oid","userpic"];
+const exceptions = ["Jinja2","asc","bic","iban"];
 
-const codeStyleRegex = /[_]/        // snake_case
+const separatorsRegex = /\s/     // any whitespace
 
 export default (input) => {
 
-    const words = input.split(codeStyleRegex);
-    const mistakes = words
-        .filter((word) => !exceptions.includes(word))
-        .filter((word) => spellChecker.isMisspelled(word));
+  const words = input.replace(/`/g, '').split(separatorsRegex);
+  const mistakes = words
+    .filter((word) => !exceptions.includes(word))
+    .filter((word) => spellChecker.isMisspelled(word));
 
-    if (mistakes.length > 0) {
-        return [{
-            message: `Spelling mistakes found: ${mistakes.join(', ')}`,
-        }];
-    }
+  if (mistakes.length > 0) {
+    return [{
+      message: `Spelling mistakes found: ${mistakes.join(', ')}`,
+    }];
+  }
 };
