da1d656beb7d70f7cd0cdc75688d1071b31aefd9
daniel-dierich <147144836+daniel-dierich@users.noreply.github.com>
2023-10-09 14:26:37 +0200

    Update check-spelling-code.js

--- lint_functions/check-spelling-code.js
+++ lint_functions/check-spelling-code.js
@@ -5,17 +5,15 @@ const separatorsRegex = /\s/     // any whitespace
 
 export default (input) => {
   console.log("NEW INPUT:       " + String(input));
-  if (typeof input === 'string' || input instanceof String){
-    const words = input.replace(/`/g, '').split(separatorsRegex);
-    const mistakes = words
-      .filter((word) => !exceptions.includes(word))
-      .filter((word) => spellChecker.isMisspelled(word));
+  const words = input.replace(/`/g, '').split(separatorsRegex);
+  const mistakes = words
+    .filter((word) => !exceptions.includes(word))
+    .filter((word) => spellChecker.isMisspelled(word));
   
 
-    if (mistakes.length > 0) {
-      return [{
-        message: `Spelling mistakes found: ${mistakes.join(', ')}`,
-      }];
-    }
+  if (mistakes.length > 0) {
+    return [{
+      message: `Spelling mistakes found: ${mistakes.join(', ')}`,
+    }];
   }
 };
