3c39c33e43ea52b8b37a8ecc601e83a86012dfa9
daniel-dierich <147144836+daniel-dierich@users.noreply.github.com>
2023-10-10 10:06:26 +0200

    Try different lib

--- lint_functions/check-spelling-code.js
+++ lint_functions/check-spelling-code.js
@@ -1,6 +1,9 @@
 const spellChecker = require('spellchecker');
+const spellchecker = require('simple-spellchecker');
 const exceptions = ["Jinja2","asc","bic","iban"];
 
+
+var dictionary = spellchecker.getDictionarySync("fr-FR"); 
 const separatorsRegex = /\s/     // any whitespace
 
 export default (input) => {
@@ -8,7 +11,7 @@ export default (input) => {
   
   const mistakes = words
     .filter((word) => !exceptions.includes(word))
-    .filter((word) => spellChecker.isMisspelled(word));
+    .filter((word) => spellchecker.isMisspelled(word));
 
   if (mistakes.length > 0) {
     console.log(mistakes);
