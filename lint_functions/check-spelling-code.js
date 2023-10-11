daff865f5d2d6547426f716601e7ea4ca2fa2b9e
daniel-dierich <147144836+daniel-dierich@users.noreply.github.com>
2023-10-11 15:47:12 +0200

    Update check-spelling-code.js

--- lint_functions/check-spelling-code.js
+++ lint_functions/check-spelling-code.js
@@ -8,23 +8,22 @@ function includesNumber(value) {
     return /\d/.test(value);
 }
 
-export default (input) => {
-    var dict = dictionary();
-    while (!dict) {
-    }
-
-    var spell = nspell(dict);
-    var no_special_characters = input.replace(/[^\w\s]/gi, '');
-    const words = no_special_characters.split(separatorsRegex);
-    var errors = words
-        .filter((word) => !exceptions.includes(word))
-        .filter((word) => !spell.correct(word))
-        .filter((word) => word != '')
-        .filter((word) => !includesNumber(word));
-
-    if (errors.length > 0) {
-        mistakes.push(errors);
-        console.log("Es wurde ein Rechtschreibfehler gefunden: " + errors);
-    }
-    return mistakes;
-}
+export default (input) =>{
+    dictionary ((err, dict) => {
+        if (err) {
+            throw err;
+        }
+        var spell = nspell(dict)
+        var no_special_characters= input.replace(/[^\w\s]/gi, '')
+        const words = no_special_characters.split(separatorsRegex);
+        var errors= words
+          .filter((word) => !exceptions.includes(word))
+          .filter((word) => !spell.correct(word))
+          .filter((word) => word!='')
+          .filter((word) => !includesNumber(word));
+                if (errors.length > 0) {
+            mistakes.push(errors);
+            console.log("There was a spelling mistake found: " + errors)
+        }
+    })
+};
