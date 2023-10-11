1c8e757bcf02aa069f920a0bf1efbf3a163e7b15
daniel-dierich <147144836+daniel-dierich@users.noreply.github.com>
2023-10-11 11:23:30 +0200

    Update check-spelling-code.js

--- lint_functions/check-spelling-code.js
+++ lint_functions/check-spelling-code.js
@@ -26,7 +26,7 @@ export default (input) =>{
         if (errors.length > 0) {
             mistakes.push(errors);
         }
-        console.log(mistakes);
         return mistakes;
     }
+    console.log(mistakes);
 };
