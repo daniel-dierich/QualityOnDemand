5ea9c21184dd05e89addea9984175a77d81d1fb8
daniel-dierich <147144836+daniel-dierich@users.noreply.github.com>
2023-10-11 10:46:01 +0200

    Update check-spelling-code.js

--- lint_functions/check-spelling-code.js
+++ lint_functions/check-spelling-code.js
@@ -8,9 +8,7 @@ function includesNumber(value) {
     return  /\d/.test(value);
 }
 export default (input) =>{
-    dictionary(var x= ondictionary;);
-    return x;
-    
+    dictionary(ondictionary);
     function ondictionary(err, dict) {
         if (err) {
             throw err;
@@ -28,8 +26,6 @@ export default (input) =>{
         if (errors.length > 0) {
             mistakes.push(errors);
         }
-        if (mistakes.length > 0){
-            return [{ message: `Spelling mistakes found: ${mistakes.join(', ')}`,}];
-        }
     }
+    return mistakes;
 };
